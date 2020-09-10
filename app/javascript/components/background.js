8ee9a0391af8c94132ce60a7ee20bc72efda4e4f
Grace Teng <grace@pelicularities.net>
2020-09-10 19:06:51 +0800

    modified CSS. to be continued tomorrow

--- app/javascript/components/background.js
+++ app/javascript/components/background.js
@@ -1,7 +1,7 @@
 const randomCocktailBackground = () => {
   const maxNum = 11;
   let num = Math.ceil(Math.random() * maxNum);
-  const assetPath = `assets/backgrounds/background${num}.jpg`;
+  const assetPath = `/assets/backgrounds/background${num}.jpg`;
   document.body.style.backgroundImage = `url(${assetPath})`;
 }
 
