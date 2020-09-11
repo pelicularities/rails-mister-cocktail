679223192996d4262b12eb9239cdb4755628aebe
Grace Teng <grace@pelicularities.net>
2020-09-11 15:01:31 +0800

    moved random background generation from JS to SCSS

--- app/javascript/components/background.js
+++ app/javascript/components/background.js
@@ -2,7 +2,7 @@ const randomCocktailBackground = () => {
   const maxNum = 11;
   let num = Math.ceil(Math.random() * maxNum);
   const assetPath = `/assets/backgrounds/background${num}.jpg`;
-  document.body.style.backgroundImage = `url(${assetPath})`;
+  document.body.style.backgroundImage = url(assetPath);
 }
 
 export { randomCocktailBackground }
